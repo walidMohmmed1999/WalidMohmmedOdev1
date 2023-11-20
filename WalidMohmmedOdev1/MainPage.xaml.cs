ab6e6d149b51a50f983296de8c3a3985e6134a03
WALID-MOHMMED <20519514665@ogrenci.bartin.edu.tr>
2023-11-20 21:37:52 +0300

    Proje dosyası ekle.

---
+++ WalidMohmmedOdev1/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace WalidMohmmedOdev1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+ 
+    }
+}
